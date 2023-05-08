6bfbcd1102468e2c68d368694c1f1b0ab1980444
sungohki <sokim1201@naver.com>
2023-05-08 21:05:03 +0900

    modified : practice for Widget class

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
+  // App == root of app ui
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Hello Flutter'),
+            ),
+            body: Center(
+              child: Text("Hello, World!"),
+            ))));
+  }
 }

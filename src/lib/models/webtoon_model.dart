77c20bab3af7272c16b549e30deb91295e148630
sungohki <sokim1201@naver.com>
2023-05-14 05:42:40 +0900

    modified : add webtoon_model class contents

--- src/lib/models/webtoon_model.dart
+++ src/lib/models/webtoon_model.dart
@@ -1,1 +1,8 @@
-class WebtoonModel {}
+class WebtoonModel {
+  final String title, thumb, id;
+
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        thumb = json['thumb'],
+        id = json['id'];
+}
